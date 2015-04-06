30d62b3d0fdee7339071c73fa80a7b95291ecd52
Froilan Irizarry <irizarry.froilan@gmail.com>
2015-04-06 05:08:04 -0400

    Added MongoKit to project. Added MongoKit to requirements.txt. Created Query model in db.py. Refactored pymongo syntax to Query model actions. Added date_created and date_updated in Query model. Added date_created and date_updated in view_query.html.

--- db.py
+++ db.py
@@ -1,44 +1,67 @@
-import pymongo as m
+# import pymongo as m
+from datetime import datetime
+from mongokit import Connection, Document
 from bson.objectid import ObjectId
 
 import config
 
-client = m.MongoClient(config.mongo_hostname, config.mongo_port)
-db = client[config.mongo_db]
-queries = db[config.mongo_collection]
+connection = Connection(host=config.mongo_hostname, port=config.mongo_port)
+
+@connection.register
+class Query(Document):
+    __database__ = 'company_queries'
+    __collection__ = 'queries'
+    _obj_class = Document  # TODO: find out why an error shows up when this isn't here. Should be inhereted from Document.
+
+    structure = {
+        'title': basestring,
+        'sql': basestring,
+        'tags': [basestring],
+        'desc': basestring,
+        'who': basestring,
+        'date_created': datetime,
+        'date_updated': datetime,
+    }
+    required_fields = ['title', 'sql', 'desc', 'who']
+    default_values = {
+        'date_created': datetime.utcnow
+    }
+    use_dot_notation = True
 
 def get_tags_list(tags):
     return [ tag.strip() for tag in tags.strip().split(",") \
             if len(tag.strip()) > 0]
 
 def insert_query(title, sql, tags, desc, who):
-    _tags = get_tags_list(tags)
-    queries.insert({
-            "title": title,
-            "sql": sql,
-            "tags": _tags,
-            "desc": desc,
-            "who": who
-        })
+
+    query = connection.Query()
+    
+    query.title = title
+    query.sql = sql
+    query.tags = get_tags_list(tags)
+    query.desc = desc
+    query.who = who
+    query.save()
+    
 
 def update_query(id, title, sql, tags, desc, who):
-    _tags = get_tags_list(tags)
-    queries.update({
-        "_id": ObjectId(id)
-        },
-        { "$set" : {
-            "title": title,
-            "sql": sql,
-            "tags": _tags, 
-            "desc": desc,
-            "who": who
-            }}, upsert=False)
+    query = connection.Query.find_one(ObjectId(id))
+    
+    query.title = title
+    query.sql = sql
+    query.tags = get_tags_list(tags)
+    query.desc = desc
+    query.who = who
+    query.date_updated = datetime.utcnow()
+
+    query.save()
 
 def delete_query(id):
-    queries.remove({"_id": ObjectId(id)})
+    connection.Query.find_one(ObjectId(id)).delete()
+    # queries.remove({"_id": ObjectId(id)})
 
 def get_queries():
-    return list(queries.find())
+    return list(connection.Query.find())
 
 def get_query_details(id):
-    return queries.find_one(ObjectId(id))
+    return connection.Query.find_one(ObjectId(id))
