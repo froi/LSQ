7ff942840e1e3127950a47f1db4a829f0295c0ba
Froilan Irizarry <irizarry.froilan@gmail.com>
2015-05-06 16:01:47 -0400

    Added login and registration forms. Added login and registration templates. Added flask-login and flask-wtf to requirements. Changed Query class for Snippets class. Changed all query methods to snippet methods. Added snippet types. Who fields will now be filled with the current loged in user. Who field was changd for snippet type field in templates. Huge refactor of python code.

--- db.py
+++ db.py
@@ -9,8 +9,43 @@ import config
 connection = Connection(host=config.mongo_hostname, port=config.mongo_port)
 
 @connection.register
+class User(Document):
+    __database__ = 'company_snippets'
+    __collection__ = 'users'
+    _obj_class = Document
+
+    structure = {
+        'username': basestring,
+        'password': basestring,
+        'email': basestring,
+        'fname': basestring,
+        'lname': basestring,
+        'date_created': datetime,
+        'last_login': datetime,
+    }
+
+    required_fields = ['username', 'password', 'email']
+    use_dot_notation = True
+ 
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self._id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+@connection.register
 class Comment(Document):
-    __database__ = 'company_queries'
+    __database__ = 'company_snippets'
     __collection__ = 'comments'
     _obj_class = Document
 
@@ -23,15 +58,17 @@ class Comment(Document):
     required_fields = ['text', 'who']
     use_dot_notation = True
 
+
 @connection.register
-class Query(Document):
-    __database__ = 'company_queries'
-    __collection__ = 'queries'
+class Snippet(Document):
+    __database__ = 'company_snippets'
+    __collection__ = 'snippets'
     _obj_class = Document  # TODO: find out why an error shows up when this isn't here. Should be inhereted from Document.
 
     structure = {
         'title': basestring,
-        'sql': basestring,
+        'text': basestring,
+        'type': basestring,
         'tags': [basestring],
         'desc': basestring,
         'who': basestring,
@@ -39,57 +76,58 @@ class Query(Document):
         'date_updated': datetime,
         'comments': [Comment]
     }
-    required_fields = ['title', 'sql', 'desc', 'who']
+    required_fields = ['title', 'text', 'type', 'desc', 'who']
     use_dot_notation = True
     use_autorefs = True
-
 
 def get_tags_list(tags):
     return [ tag.strip() for tag in tags.strip().split(",") \
             if len(tag.strip()) > 0]
 
-def insert_query(title, sql, tags, desc, who):
+def insert_snippet(title, snippet_text, snippet_type, tags, desc, who):
 
-    query = connection.Query()
+    snippet = connection.Snippet()
+    
+    snippet.title = title
+    snippet.text = snippet_text
+    snippet.type = snippet_type
+    snippet.tags = get_tags_list(tags)
+    snippet.desc = desc
+    snippet.who = who
+    snippet.date_created = datetime.utcnow()
+    snippet.date_updated = datetime.utcnow()
+    snippet.save()
+
+def update_snippet(snippet_id, title, sql, tags, desc, who):
+    snippet = connection.snippet.find_one(ObjectId(snippet_id))
     
     query.title = title
-    query.sql = sql
+    query.text = snippet
+    query.type = snippet_type
     query.tags = get_tags_list(tags)
     query.desc = desc
     query.who = who
-    query.date_created = datetime.utcnow()
-    query.date_updated = datetime.utcnow()
-    query.save()
-
-def update_query(id, title, sql, tags, desc, who):
-    query = connection.Query.find_one(ObjectId(id))
-    
-    query.title = title
-    query.sql = sql
-    query.tags = get_tags_list(tags)
-    query.desc = desc
-    query.who = who
     query.date_updated = datetime.utcnow()
 
     query.save()
 
-def delete_query(id):
-    connection.Query.find_one(ObjectId(id)).delete()
+def delete_snippet(snippet_id):
+    connection.Snippet.find_one(ObjectId(snippet_id)).delete()
 
-def get_queries():
-    return list(connection.Query.find())
+def get_snippets():
+    return list(connection.Snippet.find())
 
-def get_query_details(id):
-    query = connection.Query.find_one(ObjectId(id))
-    query.desc = markdown(query.desc)
+def get_snippet_details(snippet_id):
+    snippet = connection.Snippet.find_one(ObjectId(snippet_id))
+    snippet.desc = markdown(snippet.desc)
     tmp_comments = []
 
     for comment in query.comments:
         comment.text = markdown(comment.text)
         tmp_comments.append(comment)
 
-    query.comments = tmp_comments
-    return query
+    snippet.comments = tmp_comments
+    return snippet
 
 def insert_comment(comment_text, who):
 
@@ -104,8 +142,8 @@ def insert_comment(comment_text, who):
 
     return comment
 
-def update_comment(id, comment_text, who):
-    comment = connection.Comment.find_one(ObjectId(id))
+def update_comment(comment_id, comment_text, who):
+    comment = connection.Comment.find_one(ObjectId(comment_id))
 
     comment.text = comment_text
     comment.who = who
@@ -113,8 +151,8 @@ def update_comment(id, comment_text, who):
 
     comment.save()
 
-def delete_comment(id):
-    connection.Comment.find_one(ObjectId(id)).delete()
+def delete_comment(comment_id):
+    connection.Comment.find_one(ObjectId(comment_id)).delete()
 
 def get_comments():
     tmp_comments = []
@@ -125,3 +163,23 @@ def get_comments():
         tmp_comments.append(comment)
 
     return tmp_comments
+
+def get_user(user_id=None, username=None):
+    if user_id:
+        user = connection.User.find_one(ObjectId(user_id))
+    elif username:
+        user = connection.User.find_one({'username': username})
+    else:
+        pass  # TODO: what to do here...raise error or return None?
+    return user
+
+def add_user(username, password, email=None, fname=None, lname=None):
+    user = connection.User()
+
+    user.username = username
+    user.password = password
+    user.email = email
+    user.fname = fname
+    user.lname = lname
+
+    user.save()
